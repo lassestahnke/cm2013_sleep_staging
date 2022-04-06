b048d6441401a76d1677bfc08419a925de16e3ad
Franckie <fd.dejaegere@gmail.com>
2022-04-06 15:32:25 +0200

    Modification and finalisation of Hjort and temporal features extraction

---
+++ extract_hjort_parameters.m
@@ -0,0 +1,18 @@
+function [mobility, complexity] = extract_hjort_parameters(signal, epochLength, Fs)
+% Function calculating Hjort parameters of a signal
+numberOfEpochs = floor(length(signal)/epochLength/Fs);
+
+activity = zeros(1, numberOfEpochs);
+mobility= zeros(1, numberOfEpochs);
+complexity= zeros(1, numberOfEpochs);
+
+for epochNumber=1:numberOfEpochs
+    epochStart = ((epochNumber-1)*Fs*epochLength+1);
+    epochEnd = (epochStart-1) + epochLength*Fs ;
+    activity = var(signal(epochStart:epochEnd));
+
+    mobility(epochNumber) = sqrt(var(gradient(signal(epochStart:epochEnd)))/activity);
+    complexity(epochNumber) = (sqrt(var(gradient(gradient(signal(epochStart:epochEnd)))))/var(signal(epochStart:epochEnd)))/mobility(epochNumber);
+end
+
+end
--- extract_temp_features.m
+++ extract_temp_features.m
@@ -1,16 +1,21 @@
-function [mean_signal, variance, amplitude] = extract_temp_features(signal, epochLength, numberOfEpochs, Fs)
-length_signal =  length(signal);
-%numberOfEpochs = length_signal/epochLength;
+function [mean_signal, variance, amplitude, skewness_signal, kurtosis_signal] = extract_temp_features(signal, epochLength, Fs)
+
+numberOfEpochs = floor(length(signal)/epochLength/Fs);
 mean_signal= zeros(1, numberOfEpochs);
 variance = zeros(1, numberOfEpochs);
 amplitude= zeros(1, numberOfEpochs);
+skewness_signal= zeros(1, numberOfEpochs);
+kurtosis_signal= zeros(1, numberOfEpochs);
 
 for epochNumber=1:numberOfEpochs
-    epochStart = (epochNumber*Fs);
-    epochEnd = epochStart + 30*Fs;
+    epochStart = ((epochNumber-1)*Fs*epochLength+1);
+    epochEnd = (epochStart-1) + epochLength*Fs ;
+
     mean_signal(epochNumber)=mean(signal(epochStart:epochEnd));
     variance(epochNumber)=var(signal(epochStart:epochEnd));
     amplitude(epochNumber)=max(signal(epochStart:epochEnd))-min(signal(epochStart:epochEnd)); % Defined as maximum delta in a 
+    skewness_signal(epochNumber)=skewness(signal(epochStart:epochEnd));
+    kurtosis_signal(epochNumber)=kurtosis(signal(epochStart:epochEnd));
 end
 return
 
